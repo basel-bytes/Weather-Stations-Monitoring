588b264315e8aa6e83f15ebff195430cdb3173e3
LouayMagdy <magdylouay@gmail.com>
2023-05-21 21:11:06 +0300

    adding program arguments

--- weather-station/src/main/java/Main.java
+++ weather-station/src/main/java/Main.java
@@ -1,26 +1,11 @@
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import station.Station;
 import station.StationFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Station station = StationFactory.generateStation(new String[]{"Mock"});
+        Station station = StationFactory.generateStation(args);
         station.SendMessages();
-
-//        Properties props = new Properties();
-//        InputStream kafkaIS = Main.class.getClassLoader().getResourceAsStream("kafkaConfig.properties");
-//        props.load(kafkaIS);
-//
-//        String topicName = props.get("topic").toString();
-//        props.remove("topic");
-//        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
-//        ProducerRecord<String, String> record = new ProducerRecord<>(topicName, "Heeeeeeeelllllo");
-//        kafkaProducer.send(record);
-//        kafkaProducer.close();
     }
 }
