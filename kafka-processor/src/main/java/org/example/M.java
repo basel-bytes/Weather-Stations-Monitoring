fe63ca49d30634432c91e81f87ac20fb2417d726
ziad40 <zizoabuelkher@gmail.com>
2023-05-21 12:58:38 +0300

    first version of kafka processor, we need to merge with moch station instead of trials I make using class sample and M

--- kafka-processor/src/main/java/org/example/M.java
+++ kafka-processor/src/main/java/org/example/M.java
@@ -1,2 +1,10 @@
-package org.example;public class M {
+package org.example;
+
+public class M {
+    int x;
+    String s;
+    public M(int x,String s){
+        this.x = x;
+        this.s = s;
+    }
 }
--- kafka-processor/src/main/java/org/example/Main.java
+++ kafka-processor/src/main/java/org/example/Main.java
@@ -1,10 +1,9 @@
-package org.example;
+package src.main.java.org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
         }
     }
-}
