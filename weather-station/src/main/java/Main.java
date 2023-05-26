26bf3af71e97059789521e5718317cfc1f025d58
LouayMagdy <magdylouay@gmail.com>
2023-05-26 16:40:06 +0300

    adding Dockerfile

--- weather-station/src/main/java/Main.java
+++ weather-station/src/main/java/Main.java
@@ -2,10 +2,13 @@ import station.Station;
 import station.StationFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Station station = StationFactory.generateStation(args);
         station.SendMessages();
     }
+
 }
